9865fa29d6e5780b66da15173cd7325ad4187c27
Christian James Attilio Gioia <ChristianJamesAttilio.Gioia@team.telstra.com>
2020-07-27 11:29:03 +1000

    First commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Persona 5  <code>is very very very good. </code> and dfdfgfgfdgf to reload.
         </p>
         <a
           className="App-link"
